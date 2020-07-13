d9d76d932daffb44c52d9a128bfa40ba46a9026c
Phillip Rust <phillip.rust@web.de>
2020-07-13 23:28:25 +0200

    Refactor app.py

--- app.py
+++ app.py
@@ -1,84 +1,76 @@
+import json
+import pickle
+import numpy as np
+import torch
+from torch import load
+import torchvision
 from flask import Flask
 from flask import request
-import pickle
-import torchvision
-
-import numpy as np
-
-from load_model import load_model
-
-import json
-
-from torch import load
-import torch
-
+from flask_cors import CORS
 from PIL import Image
 import matplotlib.pyplot as plt
 
+from load_model import load_model
 
 app = Flask(__name__)
 
-from flask_cors import CORS
 CORS(app)
 h = 256
 w = h
 
+"""
+This flask application provides a REST endpoint for our image inpainting neural network
+"""
+
+# load model from checkpoint
 model = load_model("model_hybrid_3x3_1.16loss.pt")
 
 @app.route('/', methods=["GET", "POST"])
-def hello_world():
+def get_prediction():
+    """
+    Prediction endpoint (POST) which takes image and mask and returns inpainted image
+    """
+
     if request.method == "GET":
         return 'Please use POST and submit the image and mask as json'
-    if request.method == "POST":
-        # print(request.data)
-        img, mask = get_image_and_mask_from(request.data)
 
+    if request.method == "POST":
+        
+        # get inputs
+        img, mask = get_image_and_mask_from(request.data)
         img = np.array(img, dtype=np.uint8)
-        plt.imsave("input.png", img)
+
+        # prepare mask for model
         mask = np.array(mask, dtype=np.uint8, ndmin=2)
         mask = np.reshape(mask, (h, w, 1))
-
-        print(np.unique(mask.reshape(256*256)))
-
-
         mask = np.negative(mask-1)
-        plt.imsave("mask.png", np.reshape(mask.astype("float"), (h,w)))
         mask = torch.Tensor(np.transpose(mask, (2, 0, 1)))
 
-
+        # prepare image for model and concatenate with mask
         transformed_image = transform(img)
-        print(mask.shape)
-        print(transformed_image.shape)
         input_ = torch.cat((transformed_image, mask), 0)
         input_ = torch.reshape(input_, (1, 4, h, w))
-        print(input_.shape)
-        print(input_)
-        #print(input_)
-        np_pred = (model(input_)*255).detach().numpy()
-        print(np_pred)
-        print(np_pred.shape)
-        np_pred = np.transpose(np_pred, (0,2,3,1)) 
-        print(np_pred.shape)
-        np_pred = np_pred.reshape((256,256,3))
-        print(np_pred.shape)
 
+        # get model prediction
+        np_pred = (model(input_)*255).detach().numpy()
+
+        # prepare prediction to send back to client
+        np_pred = np.transpose(np_pred, (0,2,3,1)) 
+        np_pred = np_pred.reshape((256,256,3))
         np_pred = np.floor(np_pred)
-        print(np_pred)
-        plt.imsave("prediction_.jpg", np_pred.astype('uint8'))
-        #np_pred = np.absolute(np_pred-255)
         pred = np_pred.astype('uint8').tolist()
 
-        plt.imsave("prediction.jpg", pred)
-
-        #print(pred)
-
         return json.dumps(get_data_from_image(pred))
+        
     else:
         return "Invalid HTTP Method"
 
 
 def get_data_from_image(image):
-    # print(image)
+    """
+    Takes in inpainted image prediction as multidimensional list (RGB channels), extracts the data, and writes it into flat list
+    """
+
     image_height = 256
     image_width = image_height
     image_data = []
@@ -96,8 +88,11 @@ def get_data_from_image(image):
 
 
 def get_image_and_mask_from(json_string):
-    json_string = str(json_string, encoding="utf-8")
+    """
+    Takes in JSON string from client and outputs the damaged image and mask
+    """
 
+    json_string = str(json_string, encoding="utf-8")
     json_string = json_string.replace("null", "0")
 
     obj = json.loads(json_string)
@@ -115,15 +110,14 @@ def get_image_and_mask_from(json_string):
             if counter == 3:
                 mask[y][x] = 1.
 
-    for m in mask:
-        if not len(m) == 256:
-            print(m)
-            print("RIP:" + str(len(m)))
-
     return damaged_img, mask
 
 
 def get_image_from_data(data):
+    """
+    Takes in image data as 1-d list, distributes pixels among respective color channels, and returns 3-d RGB image
+    """
+
     image_height = 256
     image_width = image_height
     image = np.ndarray((256,256,3), dtype=np.uint8)
@@ -141,5 +135,9 @@ def get_image_from_data(data):
     return image
 
 def transform(image):
+    """
+    Takes in image as numpy ndarray (RGB channels), transforms it to tensor, and returns image tensor
+    """
+
     image = torchvision.transforms.ToTensor()(image)
     return image
